64c640125b61b9aeb1ff1a9d123307e5968363e1
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-30 15:27:03 -0300

    Speedup tim and turn invisible

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import turtle as turtle_module
 screen = turtle_module.Screen()
 tim = turtle_module.Turtle()
 turtle_module.colormode(255)
+tim.hideturtle()
 tim.penup()
+tim.speed('fastest')
 tim.setposition(-300, -255)
 
-for i in range(11):
+for i in range(12):
     for j in range(13):
-        tim.dot(15, choice(color_list))
+        tim.dot(18, choice(color_list))
         tim.forward(50)
     tim.setposition(-300, i * 50 - 255)
     tim.left(90)
